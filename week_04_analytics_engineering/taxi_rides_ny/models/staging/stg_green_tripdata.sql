c7a6614d4f7a30b99529b1ecdfa70161a8322a80
Faisal Qureshi <faq.dbt01@gmail.com>
2024-02-08 23:36:44 +0000

    Added Macro get_payment_type_description

--- week_04_analytics_engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ week_04_analytics_engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -1,3 +1,6 @@
 {{ config(materialized='view') }}
 
-select * from {{ source('staging', 'external_green_tripdata') }} limit 100
+select * ,
+{{ get_payment_type_description("payment_type") }} as payment_type_description
+from {{ source('staging', 'external_green_tripdata') }} 
+limit 100
