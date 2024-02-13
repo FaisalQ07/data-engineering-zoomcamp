e078ebdc9689f996c5a75095d6504a979c645f8b
Faisal Qureshi <faq.dbt01@gmail.com>
2024-02-13 18:36:46 +0000

    updated core and staging models to include yellow tripdata

--- week_04_analytics_engineering/taxi_rides_ny/models/core/fact_trips.sql
+++ week_04_analytics_engineering/taxi_rides_ny/models/core/fact_trips.sql
@@ -4,44 +4,55 @@
     )
 }}
 
-with green_tripdata as (
+with green_data as (
     select *, 
         'Green' as service_type
     from {{ ref('stg_green_tripdata') }}
 ), 
+
+yellow_data as (
+    select *, 
+        'Yellow' as service_type
+    from {{ ref('stg_yellow_tripdata') }}
+), 
+
+trips_unioned as (
+    select * from green_data
+    union all
+    select * from yellow_data
+),
  
 dim_zones as (
     select * from {{ ref('dim_zones') }}
     where borough != 'Unknown'
 )
-select green_tripdata.tripid, 
-    green_tripdata.vendorid, 
-    green_tripdata.service_type,
-    green_tripdata.ratecodeid, 
-    green_tripdata.pickup_locationid, 
+select trips_unioned.tripid, 
+    trips_unioned.vendorid, 
+    trips_unioned.service_type,
+    trips_unioned.ratecodeid, 
+    trips_unioned.pickup_locationid, 
     pickup_zone.borough as pickup_borough, 
     pickup_zone.zone as pickup_zone, 
-    green_tripdata.dropoff_locationid,
+    trips_unioned.dropoff_locationid,
     dropoff_zone.borough as dropoff_borough, 
     dropoff_zone.zone as dropoff_zone,  
-    green_tripdata.pickup_datetime, 
-    green_tripdata.dropoff_datetime, 
-    green_tripdata.store_and_fwd_flag, 
-    green_tripdata.passenger_count, 
-    green_tripdata.trip_distance, 
-    green_tripdata.trip_type, 
-    green_tripdata.fare_amount, 
-    green_tripdata.extra, 
-    green_tripdata.mta_tax, 
-    green_tripdata.tip_amount, 
-    green_tripdata.tolls_amount, 
-    green_tripdata.ehail_fee, 
-    green_tripdata.improvement_surcharge, 
-    green_tripdata.total_amount, 
-    green_tripdata.payment_type, 
-    green_tripdata.payment_type_description
-from green_tripdata
+    trips_unioned.pickup_datetime, 
+    trips_unioned.dropoff_datetime, 
+    trips_unioned.store_and_fwd_flag, 
+    trips_unioned.passenger_count, 
+    trips_unioned.trip_distance, 
+    trips_unioned.trip_type, 
+    trips_unioned.fare_amount, 
+    trips_unioned.extra, 
+    trips_unioned.mta_tax, 
+    trips_unioned.tip_amount, 
+    trips_unioned.tolls_amount, 
+    trips_unioned.improvement_surcharge, 
+    trips_unioned.total_amount, 
+    trips_unioned.payment_type, 
+    trips_unioned.payment_type_description
+from trips_unioned
 inner join dim_zones as pickup_zone
-on green_tripdata.pickup_locationid = pickup_zone.locationid
+on trips_unioned.pickup_locationid = pickup_zone.locationid
 inner join dim_zones as dropoff_zone
-on green_tripdata.dropoff_locationid = dropoff_zone.locationid
+on trips_unioned.dropoff_locationid = dropoff_zone.locationid
